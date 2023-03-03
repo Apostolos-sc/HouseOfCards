0246f66e79d360ecd3f4f44da5193c1610bb33b7
Apostolos <apostolos.scondriani@ucalgary.ca>
2023-03-03 15:50:47 -0700

    db read access function implementation user.php

--- Website/model/user.php
+++ Website/model/user.php
@@ -1,9 +1,10 @@
 <?php
     //Author            : Apostolos Scondrianis
     //Date Created      : 12-02-2023
-    //Last Edited     	: 21-02-2023
+    //Last Edited By    : Apostolos Scondrianis
+    //Last Edited On    : 03-02-2023
     //Filename          : user.php
-    //Version           : 1.2
+    //Version           : 1.3
     //Class User
     class User {
 
@@ -17,6 +18,8 @@
         private String $password;
         private Date $dob;
         private Favourite $favourites = array();
+        //Should consider if we should carry the ratings in the user for each user or if we should query the db each time
+        // we need them 
 
         //Constructor
         public function __construct(int $userID, UserType $userGroup, String $firstName, String $lastName, 
@@ -122,20 +125,97 @@
         //stubs for functions that access db
         public static function fetchUserByID(Database $dbConnection, int $userID) : ?User {
             //request a user by ID, returns null if not found
-            $user = null;
-            return $user;
+            if($dbConnection->is_connected()) {
+                $stmt = $dbConnection->connection->prepare('SELECT * FROM users WHERE users.id=?');
+                $stmt->bind_param('i', $userID);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if($result->num_rows == 0) {
+                    //no result. Return null
+                    return null;
+                } else {
+                    //fetch associatively a row. Use $row to get the data needed. Only one user should be found.
+                    $row = $result->fetch_assoc();
+                    $userType = UserType::fetchUserType($dbConnection, $row['userAccessLevel']);
+                    //Create an array of strings using - as a delimeter.
+                    $date_arr = explode ("-", $row['dob']);
+                    //Create Date object
+                    $dob = new Date($date_arr[2]+0, $date_arr[1] + 0, $date_arr[0], "00"+0, "00"+0, "00" + 0);
+                    //Get faovurites of the users
+                    $favourites = Favourite::fetchFavouritesByUserID($dbConnection, $userID);
+                    $user = new User($row['id'], $userType, $row['fname'], $row['lname'], $row['email'], 
+                                                $row['username'], $row['password'], $dob, $favourites);
+                    return $user;
+                }
+            } else {
+                //db object is not connected. Return Null.
+                return null;
+            }
         }
 
         public static function fetchUserByUsername(Database $dbConnection, String $username) : ?User {
-            //request a user by ID, returns null if not found
-            $user = null;
-            return $user;
+            //request a user by username, returns null if not found
+            if($dbConnection->is_connected()) {
+                $stmt = $dbConnection->connection->prepare('SELECT * FROM users WHERE users.username=?');
+                $stmt->bind_param('s', $username);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if($result->num_rows == 0) {
+                    //no result. Return null
+                    return null;
+                } else {
+                    //fetch associatively a row. Use $row to get the data needed. Only one user should be found.
+                    //usernames should also be unique
+                    $row = $result->fetch_assoc();
+                    $userType = UserType::fetchUserType($dbConnection, $row['userAccessLevel']);
+                    //Create an array of strings using - as a delimeter.
+                    $date_arr = explode ("-", $row['dob']);
+                    //Create Date object
+                    $dob = new Date($date_arr[2]+0, $date_arr[1] + 0, $date_arr[0], "00"+0, "00"+0, "00" + 0);
+                    //Get favourites of the user
+                    $favourites = Favourite::fetchFavouritesByUserID($dbConnection, $row['id']);
+                    $user = new User($row['id'], $userType, $row['fname'], $row['lname'], $row['email'], 
+                                                $row['username'], $row['password'], $dob, $favourites);
+                    return $user;
+                }
+            } else {
+                //db object is not connected. Return Null.
+                return null;
+            }
         }
 
-        public static function fetchUsers(Database $dbConnection) : array {
+        public static function fetchUsers(Database $dbConnection) : ?Array {
             //Query database to return all users.
             $users = [];
-            return $users;
+            if($dbConnection->is_connected()) {
+                $stmt = $dbConnection->connection->prepare('SELECT * FROM users');
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if($result->num_rows == 0) {
+                    //no return empty array
+                    return $users;
+                } else {
+                    while($row = $result->fetch_assoc()) {
+                        //fetch associatively a row. Use $row to get the data needed.
+                        $userType = UserType::fetchUserType($dbConnection, $row['userAccessLevel']);
+                        //Create an array of strings using - as a delimeter.
+                        $date_arr = explode ("-", $row['dob']);
+                        //Create Date object
+                        $dob = new Date($date_arr[2]+0, $date_arr[1] + 0, $date_arr[0], "00"+0, "00"+0, "00" + 0);
+                        //Get faovurites of the users
+                        $favourites = Favourite::fetchFavouritesByUserID($dbConnection, $row['id']);
+                        $user = new User($row['id'], $userType, $row['fname'], $row['lname'], $row['email'], 
+                                                    $row['username'], $row['password'], $dob, $favourites);
+                        //add User object to $users array.
+                        $users[] = $user;
+                    }
+                    //done fetching users from db. Return array of users.
+                    return $users;
+                }
+            } else {
+                //db object is not connected. Return Null.
+                return null;
+            }
         }
     }
 ?>
